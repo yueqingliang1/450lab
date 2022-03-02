6c0448f02e1e1fb538c026639e60c161e479c24b
yueqingliang1 <yueqingliang1@gmail.com>
2022-03-01 18:57:07 -0600

    Add the signature of waitpid()

--- user.h
+++ user.h
@@ -5,6 +5,7 @@ struct rtcdate;
 int fork(void);
 int exit(int) __attribute__((noreturn));
 int wait(int*);
+int waitpid(int, int*, int);
 int pipe(int*);
 int write(int, const void*, int);
 int read(int, void*, int);
