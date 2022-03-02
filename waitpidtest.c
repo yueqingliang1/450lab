5dd42786ef202b80b18bd5538e2221376688bfde
yueqingliang1 <yueqingliang1@gmail.com>
2022-03-01 21:37:00 -0600

    modify waitpidtest.c

---
+++ waitpidtest.c
@@ -0,0 +1,17 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int* NULL = 0x0;
+
+int main(int argc, char *argv[]) {
+    if(fork() == 0) {
+        int pid;
+        if((pid = fork()) == 0) {
+            waitpid(pid, NULL, 0);
+        } else{
+            exit(0);
+        }
+    }
+    exit(0);
+}
